4d20eb25ca1b46f6c46332c4a977cf2282cab3ec
usarana <43117189+usarana@users.noreply.github.com>
2022-08-26 19:01:02 +0300

    Code cleanup, better shell, hostnames, nano, ls, cat, mkdir, touch, uname and sh (run shell files)

--- frontend/Kernel.cs
+++ frontend/Kernel.cs
@@ -9,28 +9,36 @@ using System.Threading;
 using System.Threading.Tasks;
 using System.Windows.Forms;
 using ZCPU;
-namespace ZOS
+
+namespace ZOS.frontend
 {
     public class Kernel
     {
-        public static void kernel_read(CPU c)
+        public static string hostname = "";
+        public static string kernel = "ZOS Kernel";
+        public static string zos_ver = "0.9.4";
+        public static void RunShellFile(CPU c, string file)
         {
-
-            string input = intarrtostr(c.rd());
-            string totest = input.Split(' ')[0];
-            if (input.StartsWith("echo "))
+            string[] f = File.ReadAllLines(file);
+            foreach(var line in f)
+            {
+                kint(c, line);
+            }
+        }
+        public static void kint(CPU c, string input)
+        {
+            //string totest = input.Split(' ')[0];
+            if (input.StartsWith("echo"))
             {
                 Console.WriteLine(input.Replace("echo ", ""));
                 c.nl();
             }
-            else if (input.StartsWith("ri"))
-            {
-                c.initd(c.bitsize);
-            }
-            else if (input.StartsWith("math "))
+            //else if (input.StartsWith("ri")) c.initd(c.bitsize);
+            else if (input.StartsWith("math"))
             {
                 try
                 {
+                    //buggy
                     var result = Convert.ToUInt64((new DataTable()).Compute(input.Replace("math ", ""), ""));
                     Console.WriteLine((result).ToString());
                     c.nl();
@@ -40,68 +48,27 @@ namespace ZOS
                     c.panic(CPU.PanicType.matherror);
                 }
             }
-            else if (input == "shutdown")
-            {
-                Environment.Exit(0);
-            }
+            else if (input == "shutdown") Environment.Exit(0);
             else if (input.StartsWith("shutdown"))
             {
                 if (input.Replace("shutdown ", "") == "-r")
                 {
                     c.clear();
-                    kernel_main(c);
+                    kmain(c, hostname);
                 }
-                else
-                {
-                    Environment.Exit(0);
-                }
+                else Environment.Exit(0);
             }
-            else if (input.StartsWith("ver"))
-            {
-                Console.WriteLine("Z beta 1 codename ready");
-                c.nl();
-            }
-            else if (input.StartsWith("gTest"))
-            {
-
-                int a = Console.BufferHeight / 3;
-                Color[] cc = new Color[3] { Color.White, Color.Red, Color.Black };
-                for (int x = 0; x < Console.BufferWidth; x++)
-                {
-                    for (int y = 0; y < Console.BufferHeight; y++)
-                    {
-                        c.Display.setPixel(cc[Convert.ToInt32(Math.Floor((double)y / a))], x, y);
-                    }
-                }
-                System.Threading.Thread.Sleep(3000);
-                Color bgc = Color.Black;
-                for (int x = 0; x < Console.BufferWidth; x++)
-                {
-                    for (int y = 0; y < Console.BufferHeight; y++)
-                    {
-                        c.Display.setPixel(bgc, x, y);
-                    }
-                }
-                Console.SetCursorPosition(0, 0);
-
-
-            }
-            else if (input.StartsWith("curl"))
-            {
-                Console.WriteLine(c.Network.get(input.Replace("curl ", "")));
-
-            }
-            else if (input.StartsWith("clear"))
-            {
-                c.clear();
-            }
-            else if (input.StartsWith("exit"))
-            {
-                Environment.Exit(0);
-            }
+            //Cringe code
+            //else if (input.StartsWith("ver"))
+            //{
+            //    Console.WriteLine("Z beta 1 codename ready");
+            //    c.nl();
+            //}
+            else if (input.StartsWith("curl")) Console.WriteLine(c.Network.get(input.Replace("curl ", "")));
+            else if (input.StartsWith("clear")) c.clear();
+            else if (input.StartsWith("exit")) Environment.Exit(0);
             else if (input.StartsWith("run"))
             {
-
                 GAZ gi = new GAZ();
                 gi.Run(input.Replace("run ", ""), c);
             }
@@ -113,28 +80,89 @@ namespace ZOS
                 File.WriteAllBytes(input.Split(' ')[2], content);
                 Console.WriteLine("Done!");
             }
-        }
-        public static void kernel_main(CPU c)
-        {
+            else if (input == ("hostnamectl")) Console.WriteLine("Hostname: " + hostname);
+            else if (input.StartsWith("hostnamectl set-hostname "))
+            {
+                hostname = input.Replace("hostnamectl set-hostname ", "");
+                if (File.Exists(Directory.GetCurrentDirectory() + "\\etc\\hostname.cfg")) File.WriteAllText(Directory.GetCurrentDirectory() + "\\etc\\hostname.cfg", hostname);
+            }
+            else if (input.StartsWith("nano"))
+            {
+                //Nano n = new Nano(c, input.Replace("nano ", ""));
+                //Console.Clear();
 
+                //Very alpha function, not implemented fully yet.
+            }
+            else if (input.StartsWith("ls"))
+            {
+                foreach(var i in Directory.GetFiles(Directory.GetCurrentDirectory()))
+                {
+                    Console.WriteLine(new FileInfo(i).Name);
+                }
+                var old = Console.ForegroundColor;
+                Console.ForegroundColor = ConsoleColor.Blue;
+                foreach (var i in Directory.GetDirectories(Directory.GetCurrentDirectory()))
+                {
+                    Console.WriteLine(new DirectoryInfo(i).Name);
+                }
+                Console.ForegroundColor = old;
+            }
+            else if (input.StartsWith("cat"))
+            {
+                Console.WriteLine(File.ReadAllText(Directory.GetCurrentDirectory() + "\\" + input.Replace("cat ", "")));
+            }
+            else if (input.StartsWith("mkdir"))
+            {
+                Directory.CreateDirectory(Directory.GetCurrentDirectory() + "\\" + input.Replace("mkdir ", ""));
+            }
+            else if (input.StartsWith("touch"))
+            {
+                File.WriteAllText(Directory.GetCurrentDirectory() + "\\" + input.Replace("mkdir ", ""), "");
+            }
+            else if (input.StartsWith("sh"))
+            {
+                RunShellFile(c, Directory.GetCurrentDirectory() + "\\" + input.Replace("sh ", ""));
+            }
+            else if (input.StartsWith("uname"))
+            {
+                Console.WriteLine(kernel + " " + zos_ver);
+            }
+        }
+        public static string get_text_after_last_occurance_of_string(string a, string b)
+        {
+            return a.Substring(a.LastIndexOf(b)+b.Length, a.Length);
+        }
+        public static void kmain(CPU c, string hn)
+        {
+            hostname = hn;
             Console.WriteLine("Welcome to Z!\n");
 
             c.nl();
             while (true)
             {
-                Console.Write("> ");
-                c.memclean(0, 30);
+                var old = Console.ForegroundColor;
+                Console.ForegroundColor = ConsoleColor.Green;
+                Console.Write("root@" + hostname);
+                Console.ForegroundColor = old;
+                Console.Write(":");
+                Console.ForegroundColor = ConsoleColor.Blue;
+                string e;
+                if (Directory.GetCurrentDirectory().Contains("\\ZCPU")) e = get_text_after_last_occurance_of_string(Directory.GetCurrentDirectory(), "\\ZCPU");
+                else e = Directory.GetCurrentDirectory();
+                Console.Write(e);
+                Console.ForegroundColor = old;
+                Console.Write("$ ");
+                //Memclean is not needed here as c.prnt is deprecated
                 c.rde();
-                kernel_read(c);
+                kint(c, intarrtostr(c.rd()));
+                //IMPORTANT! If the memclean wasnt there, the new command would overwrite the new one.
+                c.memclean(0, c.rd().Length);
             }
         }
         public static string intarrtostr(long[] intarr)
         {
             string a = "";
-            foreach (long i in intarr)
-            {
-                a += (char)i;
-            }
+            foreach (long i in intarr) a += (char)i;
             return a;
         }
     }
